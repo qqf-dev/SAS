f01a5b9c912f1365ba7b565e72d16f8d571d5c6a
OuyangYifei <931249679@qq.com>
2021-01-15 22:51:30 +0800

    init lab1

---
+++ lab1/x/a.m
@@ -0,0 +1,8 @@
+clear;
+b = 5;
+b = 'hello world';
+b = 5.5;
+c = 10+1i;
+b = b*c;
+b = b+c;
+clc;
--- lab2/ex-q.m
+++
@@ -1,6 +0,0 @@
-%{
-TODO:list:
-item:1. DT signal (using function stem())
-item:2. calculate output (using function filter())
-item:3. Application: wireless channel 
-%}
